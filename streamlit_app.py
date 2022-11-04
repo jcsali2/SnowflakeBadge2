4d10727de36ae6cacb1cb8d3442b7ed44c9ae537
Janice <117245470+jcsali2@users.noreply.github.com>
2022-11-03 20:53:21 -0500

    Update streamlit_app.py
    removed blank line commands that didn't do anything

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
-streamlit.header('')
+
 streamlit.header('Breakfast Menu')
-streamlit.text('')
 streamlit.text('Omega 3 & Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
