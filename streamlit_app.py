af2fdef9bec87cbf3896a777f3b87bb8bcd7bffc
Janice <117245470+jcsali2@users.noreply.github.com>
2022-11-03 20:48:07 -0500

    Update streamlit_app.py
    Finished the menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+streamlit.header('')
 streamlit.header('Breakfast Menu')
+streamlit.text('')
 streamlit.text('Omega 3 & Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
